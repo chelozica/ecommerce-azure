93e001728b4f3ba7229dc7b8cc67b6988d7e7211
chelozica <marcelogtq@hotmail.com>
2026-05-18 17:32:14 -0300

    Add products array with sample product data

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Produto 1",
+    price: 100,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Produto 2",
+    price: 200,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Produto 3",
+    price: 300,
+    image: "https://via.placeholder.com/150"
+  }
+];
