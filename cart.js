075f060d6b77e9171da2c2db99afd5ecd1d1b20c
chelozica <marcelogtq@hotmail.com>
2026-05-18 17:43:52 -0300

    Implement cart functionality with add, save, and get

---
+++ cart.js
@@ -0,0 +1,26 @@
+function getCart() {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function addToCart(productId) {
+    let cart = getCart();
+
+    const item = cart.find(p => p.id === productId);
+
+    if (item) {
+        item.quantity += 1;
+    } else {
+        cart.push({
+            id: productId,
+            quantity: 1
+        });
+    }
+
+    saveCart(cart);
+
+    alert("Produto adicionado ao carrinho ✅");
+}
