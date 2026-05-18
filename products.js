1aa067186adffd76e76da39a42f0b87f19f0a74e
chelozica <marcelogtq@hotmail.com>
2026-05-18 18:24:07 -0300

    Update product images to use new source

--- products.js
+++ products.js
@@ -3,18 +3,18 @@ const products = [
     id: 1,
     name: "Produto 1",
     price: 100,
-    image: "https://via.placeholder.com/150"
+    image: "https://picsum.photos/200"
   },
   {
     id: 2,
     name: "Produto 2",
     price: 200,
-    image: "https://via.placeholder.com/150"
+    image: "https://picsum.photos/200"
   },
   {
     id: 3,
     name: "Produto 3",
     price: 300,
-    image: "https://via.placeholder.com/150"
+    image: "https://picsum.photos/200"
   }
 ];
