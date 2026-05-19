f2e06b9a3cb6a56c49f4f530acdc0d0bb54fa68d
chelozica <marcelogtq@hotmail.com>
2026-05-19 18:03:41 -0300

    Update product names in index.js

--- api/products/index.js
+++ api/products/index.js
@@ -1,8 +1,8 @@
 module.exports = async function (context, req) {
     const products = [
-        { id: 1, name: "Produto 1", price: 100 },
-        { id: 2, name: "Produto 2", price: 200 },
-        { id: 3, name: "Produto 3", price: 300 }
+        { id: 1, name: "Blusinha", price: 100 },
+        { id: 2, name: "Calça", price: 200 },
+        { id: 3, name: "Vestidinho", price: 300 }
     ];
 
     context.res = {
